7fadd010b468b6a1b9cee63d061ae3cc1acaf284
Misha-Bober <144991058+Misha-Bober@users.noreply.github.com>
2024-02-15 23:15:06 +0200

    Add files via upload

--- carta/js/script.js
+++ carta/js/script.js
@@ -1,131 +1,8 @@
-// const map = L.map("map", {
-//     crs: L.CRS.Simple,
-//     minZoom: -1,
-//     maxZoom: 20
-//   });
-  
-//   const bounds = [
-//     [0, 0],
-//     [607, 756]
-//   ];
-//   const image = L.imageOverlay(
-//     "img/resurs.svg",
-//     bounds
-//   ).addTo(map);
-  
-//   var marker = L.marker([357, 113]).addTo(map);
-//   marker
-//     .bindPopup(
-//       "<b>1</b>"
-//     )
-//     .openPopup();
-
-//     var marker = L.marker([350, 126]).addTo(map);
-//   marker
-//     .bindPopup(
-//       "<b>2</b>"
-//     )
-//     .openPopup();
-
-//     var marker = L.marker([347, 130]).addTo(map);
-//   marker
-//     .bindPopup(
-//       "<b>3</b>"
-//     )
-//     .openPopup();
-
-//     var marker = L.marker([314, 178]).addTo(map);
-//   marker
-//     .bindPopup(
-//       "<b>4</b>"
-//     )
-//     .openPopup();
-
-//     var marker = L.marker([293, 572]).addTo(map);
-//   marker
-//     .bindPopup(
-//       "<b>5</b>"
-//     )
-//     .openPopup();
-
-//     var marker = L.marker([369, 346.5]).addTo(map);
-//   marker
-//     .bindPopup(
-//       "<b>6</b>"
-//     )
-//     .openPopup();
-
-//     var marker = L.marker([369, 362]).addTo(map);
-//   marker
-//     .bindPopup(
-//       "<b>7</b>"
-//     )
-//     .openPopup();
-
-//     var marker = L.marker([375, 381]).addTo(map);
-//   marker
-//     .bindPopup(
-//       "<b>8</b>"
-//     )
-//     .openPopup();
-
-//     var marker = L.marker([382, 375.5]).addTo(map);
-//   marker
-//     .bindPopup(
-//       "<b>9</b>"
-//     )
-//     .openPopup();
-
-//     var marker = L.marker([383.5, 378.5]).addTo(map);
-//   marker
-//     .bindPopup(
-//       "<b>10</b>"
-//     )
-//     .openPopup();
-
-//     var marker = L.marker([385.2, 369,4]).addTo(map);
-//   marker
-//     .bindPopup(
-//       "<b>11</b>"
-//     )
-//     .openPopup();
-
-//     var marker = L.marker([390, 368]).addTo(map);
-//   marker
-//     .bindPopup(
-//       "<b>12</b>"
-//     )
-//     .openPopup();
-
-//     var marker = L.marker([391, 370.5]).addTo(map);
-//   marker
-//     .bindPopup(
-//       "<b>13</b>"
-//     )
-//     .openPopup();
-
-//     var marker = L.marker([389, 374]).addTo(map);
-//   marker
-//     .bindPopup(
-//       "<b>14</b>"
-//     )
-//     .openPopup();
-
-//     var marker = L.marker([391, 379.5]).addTo(map);
-//   marker
-//     .bindPopup(
-//       "<b>15</b>"
-//     )
-//     .openPopup();
-
-//   map.fitBounds(bounds);
-  
-
-
+document.getElementById("map").style.backgroundColor = "#000"; //додано фоновий колір для карти
 
 const map = L.map("map", {
   crs: L.CRS.Simple,
-  minZoom: -1,
+  minZoom: 0.3,
   maxZoom: 20
 });
 
@@ -134,99 +11,99 @@ const bounds = [
   [607, 756]
 ];
 const image = L.imageOverlay(
-  "img/resurs.svg",
+  "img/group-1.svg",
   bounds
 ).addTo(map);
 
 // Масив об'єктів з інформацією про мітки
 const markersData = [
   {
-    position: [357, 115],
-    popupContent: "<b>1</b>",
+    position: [337, 107],
+    popupContent: "<b>place 1</b>",
     iconUrl: 'img/heart.svg',
     link: "https://www.google.com/search?q=google&rlz=1C1GCEA_enUA1075UA1075&oq=google&gs_lcrp=EgZjaHJvbWUyBggAEEUYOTINCAEQLhjHARjRAxiABDIGCAIQRRg8MgYIAxBFGDwyBggEEEUYPDIGCAUQRRhBMgYIBhBFGEEyBggHEEUYQdIBCDI1NjRqMGo3qAIAsAIA&sourceid=chrome&ie=UTF-8"
   },
   {
-    position: [350, 126],
-    popupContent: "<b>2</b>",
+    position: [327, 126],
+    popupContent: "<b>place 2</b>",
     iconUrl: 'img/heart.svg',
     link: "https://www.example2.com"
   },
   {
-    position: [347, 130],
-    popupContent: "<b>3</b>",
+    position: [323, 132],
+    popupContent: "<b>place 3</b>",
     iconUrl: 'img/heart.svg',
     link: "https://www.example3.com"
   },
   {
-    position: [314, 178],
-    popupContent: "<b>4</b>",
+    position: [290, 176],
+    popupContent: "<b>place 4</b>",
     iconUrl: 'img/heart.svg',
     link: "https://www.example4.com"
   },
   {
-    position: [293, 572],
-    popupContent: "<b>5</b>",
+    position: [270, 575],
+    popupContent: "<b>place 5</b>",
     iconUrl: 'img/heart.svg',
     link: "https://www.example5.com"
   },
   {
-    position: [369, 346.5],
-    popupContent: "<b>6</b>",
+    position: [344, 347],
+    popupContent: "<b>place 6</b>",
     iconUrl: 'img/heart.svg',
     link: "https://www.example6.com"
   },
   {
     position: [369, 362],
-    popupContent: "<b>7</b>",
+    popupContent: "<b>place 7</b>",
     iconUrl: 'img/heart.svg',
     link: "https://www.example7.com"
   },
   {
     position: [375, 381],
-    popupContent: "<b>8</b>",
+    popupContent: "<b>place 8</b>",
     iconUrl: 'img/heart.svg',
     link: "https://www.example8.com"
   },
   {
     position: [382, 375.5],
-    popupContent: "<b>9</b>",
+    popupContent: "<b>place 9</b>",
     iconUrl: 'img/heart.svg',
     link: "https://www.example9.com"
   },
   {
     position: [383.5, 378.5],
-    popupContent: "<b>10</b>",
+    popupContent: "<b>place 10</b>",
     iconUrl: 'img/heart.svg',
     link: "https://www.example10.com"
   },
   {
     position: [385.2, 369.4],
-    popupContent: "<b>11</b>",
+    popupContent: "<b>place 11</b>",
     iconUrl: 'img/heart.svg',
     link: "https://www.example11.com"
   },
   {
     position: [390, 368],
-    popupContent: "<b>12</b>",
+    popupContent: "<b>place 12</b>",
     iconUrl: 'img/heart.svg',
     link: "https://www.example12.com"
   },
   {
     position: [391, 370.5],
-    popupContent: "<b>13</b>",
+    popupContent: "<b>place 13</b>",
     iconUrl: 'img/heart.svg',
     link: "https://www.example13.com"
   },
   {
     position: [389, 374],
-    popupContent: "<b>14</b>",
+    popupContent: "<b>place 14</b>",
     iconUrl: 'img/heart.svg',
     link: "https://www.example14.com"
   },
   {
     position: [391, 379.5],
-    popupContent: "<b>15</b>",
+    popupContent: "<b>place 15</b>",
     iconUrl: 'img/heart.svg',
     link: "https://www.example15.com"
   }
@@ -237,8 +114,8 @@ markersData.forEach(markerData => {
   const markerOptions = {
     icon: L.icon({
       iconUrl: markerData.iconUrl,
-      iconSize: [30, 30],
-      iconAnchor: [15, 15],
+      iconSize: [15, 15],
+      iconAnchor: [7.5, 7.5],
     })
   };
 
@@ -248,17 +125,17 @@ markersData.forEach(markerData => {
   marker.bindTooltip(markerData.popupContent).openTooltip();
 
   // Випливання спливаючого вікна при наведенні мишки
-  marker.on('mouseover', function(e) {
+  marker.on('mouseover', function (e) {
     this.openTooltip();
   });
 
   // Закриття спливаючого вікна при знятті мишки
-  marker.on('mouseout', function(e) {
+  marker.on('mouseout', function (e) {
     this.closeTooltip();
   });
 
   // Обробник події натискання на мітку
-  marker.on('click', function(e) {
+  marker.on('click', function (e) {
     // Відкриття посилання на новій сторінці
     window.open(markerData.link, "_blank");
   });
@@ -269,4 +146,5 @@ markersData.forEach(markerData => {
 
 // Зміна масштабу карту так, щоб зображення було повністю видиме
 map.fitBounds(bounds);
+
 
